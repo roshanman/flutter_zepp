91f678a04600537e76ee8dcc8089b8e3ee7aefea
张休明 <zhangxiuming@zepp.com>
2023-10-22 20:54:42 +0800

    初始化登录页面布局

--- lib/images.dart
+++ lib/images.dart
@@ -1,24 +1,28 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, constant_identifier_names
 
 extension LoginImages on String {
-  static String get bg_dark => 'assets/images/login/bg_dark.png';
-  static String get bg_light => 'assets/images/login/bg_light.png';
-  static String get logo => 'assets/images/login/logo.png';
+  static const String bg_dark = 'assets/images/login/3.0x/bg_dark.png';
+  static const String bg_light = 'assets/images/login/3.0x/bg_light.png';
+  static const String logo = 'assets/images/login/3.0x/logo.png';
+
+  static const String login_method_apple = 'assets/images/login/3.0x/login_method_apple.png';
+  static const String login_method_wechat = 'assets/images/login/3.0x/login_method_wechat.png';
+  static const String login_method_xiaomi = 'assets/images/login/3.0x/login_method_xiaomi.png';
 }
 
 extension TabbarImages on String {
-  static String get aura => 'assets/images/tabbar/tabbar_icon_aura.png';
-  static String get aura_unselected => 'assets/images/tabbar/tabbar_icon_aura_unselected.png';
+  static const String aura = 'assets/images/tabbar/tabbar_icon_aura.png';
+  static const String aura_unselected = 'assets/images/tabbar/tabbar_icon_aura_unselected.png';
 
-  static String get profile => 'assets/images/tabbar/tabbar_icon_profile.png';
-  static String get profile_unselected => 'assets/images/tabbar/tabbar_icon_profile_unselected.png';
+  static const String profile = 'assets/images/tabbar/tabbar_icon_profile.png';
+  static const String profile_unselected = 'assets/images/tabbar/tabbar_icon_profile_unselected.png';
 
-  static String get sleep => 'assets/images/tabbar/tabbar_icon_sleep.png';
-  static String get sleep_unselected => 'assets/images/tabbar/tabbar_icon_sleep_unselected.png';
+  static const String sleep = 'assets/images/tabbar/tabbar_icon_sleep.png';
+  static const String sleep_unselected = 'assets/images/tabbar/tabbar_icon_sleep_unselected.png';
 
-  static String get status => 'assets/images/tabbar/tabbar_icon_status.png';
-  static String get status_unselected => 'assets/images/tabbar/tabbar_icon_status_unselected.png';
+  static const String status = 'assets/images/tabbar/tabbar_icon_status.png';
+  static const String status_unselected = 'assets/images/tabbar/tabbar_icon_status_unselected.png';
 
-  static String get workout => 'assets/images/tabbar/tabbar_icon_workout.png';
-  static String get workout_unselected => 'assets/images/tabbar/tabbar_icon_workout_unselected.png';
+  static const String workout = 'assets/images/tabbar/tabbar_icon_workout.png';
+  static const String workout_unselected = 'assets/images/tabbar/tabbar_icon_workout_unselected.png';
 }
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,25 +1,212 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../tabbar.dart';
+import '../../images.dart';
+
+final _checkboxProvider = StateProvider<bool>((ref) => false);
 
 class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) {
-                return const MainTabBar();
-              }),
-            );
-          },
-          child: const Text("Login"),
+    final bool isChecked = ref.watch(_checkboxProvider);
+
+    final licenseCheckWidget = Container(
+      padding: const EdgeInsets.only(bottom: 10),
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Material(
+            color: Colors.transparent,
+            child: Checkbox(
+              value: isChecked,
+              onChanged: (_) {
+                ref.read(_checkboxProvider.notifier).state = !isChecked;
+              },
+            ),
+          ),
+          Flexible(
+            child: RichText(
+              maxLines: 2,
+              text: TextSpan(
+                text: '我已阅读并同意 ',
+                style: const TextStyle(color: Colors.black),
+                children: <TextSpan>[
+                  const TextSpan(
+                    text: '软件许可及服务协议',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  const TextSpan(text: ' 和 '),
+                  TextSpan(
+                    text: '隐私政策',
+                    style: const TextStyle(color: Colors.blue),
+                    recognizer: TapGestureRecognizer()..onTap = () {},
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+
+    final thirdAuthWidget = [
+      const SizedBox(height: 20),
+      Row(
+        children: [
+          const Expanded(child: Divider()),
+          const SizedBox(width: 8),
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width - 60,
+            ),
+            child: const Text(
+              maxLines: 3,
+              '第三方账号授权登录',
+            ),
+          ),
+          const SizedBox(width: 8),
+          const Expanded(child: Divider()),
+        ],
+      ),
+      const SizedBox(height: 10),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(LoginImages.login_method_apple, width: 40, height: 40),
+          ),
+          const SizedBox(width: 12),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(LoginImages.login_method_wechat, width: 40, height: 40),
+          ),
+          const SizedBox(width: 12),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(LoginImages.login_method_xiaomi, width: 40, height: 40),
+          ),
+        ],
+      ),
+    ];
+
+    final buttonsWidget = [
+      Row(
+        children: [
+          TextButton(
+            style: TextButton.styleFrom(padding: EdgeInsets.zero),
+            onPressed: () {},
+            child: const Text("忘记密码?"),
+          ),
+          const Spacer()
+        ],
+      ),
+      ElevatedButton(
+        onPressed: () => _loginButtonTapped(context),
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ),
+        child: Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          child: const Text('登录'),
         ),
       ),
+      const SizedBox(height: 10),
+      OutlinedButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ),
+        child: Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          child: const Text('注册'),
+        ),
+      ),
+    ];
+
+    final textFieldWidget = [
+      const SizedBox(height: 20),
+      TextField(
+        decoration: InputDecoration(
+          hintText: "请输入手机号/邮箱号",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+        ),
+        style: Theme.of(context).textTheme.labelLarge,
+      ),
+      const SizedBox(height: 20),
+      TextField(
+        obscureText: true,
+        enableSuggestions: false,
+        autocorrect: false,
+        decoration: InputDecoration(
+          hintText: "请输入密码",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+        ),
+        style: Theme.of(context).textTheme.labelLarge,
+      ),
+    ];
+
+    return Stack(
+      children: [
+        Positioned.fill(child: Image.asset(LoginImages.bg_light, fit: BoxFit.cover)),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_back),
+            ),
+            backgroundColor: Colors.transparent,
+          ),
+          body: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 20),
+                      Image.asset(LoginImages.logo, width: 60, height: 60),
+                      ...textFieldWidget,
+                      ...buttonsWidget,
+                      ...thirdAuthWidget,
+                    ],
+                  ),
+                ),
+              ),
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: licenseCheckWidget,
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _loginButtonTapped(BuildContext context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) {
+        return const MainTabBar();
+      }),
     );
   }
 }
